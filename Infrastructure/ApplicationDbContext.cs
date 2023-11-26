12d131091489c3f35110685e719dbdf6f18657e1
berkin <berkin-akkaya@hotmail.com>
2023-11-26 14:31:27 +0300

    migration Added - postgre connect

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -1,27 +1,28 @@
 ﻿using System;
 using KocUniversityCourseManagement.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace KocUniversityCourseManagement.Infrastructure
 {
     public class ApplicationDbContext : DbContext
     {
+        private readonly IConfiguration _configuration;
         public ApplicationDbContext() { }
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration)
             : base(options)
         {
+            _configuration = configuration;
         }
         public DbSet<ApplicationUser> Users { get; set; }
 
-
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-           if (!optionsBuilder.IsConfigured)
+            if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseNpgsql("PostgresConnection");
+                string connstr = "Host=localhost:5432;Port=5432;Database=CourseDb;User Id=postgres_user;Password=postgres_password;";
+                optionsBuilder.UseNpgsql(connstr);
             }
-          
         }
     }
     
